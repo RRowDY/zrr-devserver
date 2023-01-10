2107bb6040d48319ae3b6ab67ad86e266fefbe7d
RRowDY <durellwalzer@gmail.com>
2023-01-09 23:11:29 -0700

    Write manifest

--- resources/[rr]/circle-minigame/fxmanifest.lua
+++ resources/[rr]/circle-minigame/fxmanifest.lua
@@ -6,13 +6,16 @@ description 'Circle mini-game for Zenath'
 version '1.0'
 
 client_scripts {
-
+    'client/*.lua'
 }
 
 ui_page {
-
+    'html/index.html'
 }
 
 files {
-    
+    'html/config/circle_config.json',
+    'html/js/circle_minigame.js',
+    'html/index.html',
+    'html/style.css'
 }
